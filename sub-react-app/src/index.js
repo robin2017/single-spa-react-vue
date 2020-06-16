adfe4f13a34011378db1bda0e361cf7302d40769
robin2017 <robin.seu@foxmail.com>
2020-06-16 19:42:44 +0800

    子应用改造完成

--- sub-react-app/src/index.js
+++ sub-react-app/src/index.js
@@ -1,4 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import singleSpaReact from "single-spa-react";
 import App from './App'
-ReactDOM.render(<App/>,document.querySelector('#root'))
+
+const domElementGetter = () => document.querySelector('#single-content')
+if (!window.singleSpaNavigate) {
+    console.log('独立模式')
+    ReactDOM.render(<App/>, document.querySelector('#root'))
+} else {
+    console.log('集成模式')
+}
+const reactLifeCycles = singleSpaReact({
+    React,
+    ReactDOM,
+    rootComponent: App,
+    domElementGetter
+})
+//导出生命周期钩子
+export const bootstrap = reactLifeCycles.bootstrap
+export const mount = reactLifeCycles.mount
+export const unmount = reactLifeCycles.unmount
+//挂载到window变量上
+window.singleReact = {
+    bootstrap,
+    mount,
+    unmount
+}
--- sub-vue-app/src/main.js
+++ sub-vue-app/src/main.js
@@ -1,8 +1,39 @@
 import Vue from 'vue'
 import App from './App.vue'
+import singleSpaVue from 'single-spa-vue'
 
 Vue.config.productionTip = false
+const appOptions = {
+    pEl: '#single-content',
+    render: h => h(App),
+}
+if (!window.singleSpaNavigate) {
+    console.log('独立模式')
+    delete appOptions.pEl
+    new Vue(appOptions).$mount('#app')
+} else {
+    console.log('集成模式')
+}
+const vueLifeCycles = singleSpaVue({
+    Vue,
+    appOptions
+})
+//导出生命周期钩子
+export const bootstrap = vueLifeCycles.bootstrap
+export const mount = (props) => {
+    //在挂载点下添加vue挂载点
+    let dom = document.querySelector(appOptions.pEl)
+    let id = `randomId-${Math.random().toString(16).slice(5)}`
+    appOptions.el = `#${id}`
+    dom.innerHTML = `<div id="${id}"></div>`
+    return vueLifeCycles.mount(props)
+}
+export const unmount = vueLifeCycles.unmount
+//挂载到window变量上
+window.singleVue = {
+    bootstrap,
+    mount,
+    unmount
+}
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+
